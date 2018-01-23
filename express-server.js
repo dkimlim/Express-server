1827e357f272f4800ed0be9a10ada355f283a021
Diane KL <dianekimlim@gmail.com>
2018-01-23 17:06:08 +0000

    added a simple HTML page within javascript file...to separate later

--- express-server.js
+++ express-server.js
@@ -15,6 +15,14 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/hello", (req, res) => {
+	res.end(`<html>
+				<body>
+					Hello <b>World</b>
+				</body>
+			</html>\n`);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
