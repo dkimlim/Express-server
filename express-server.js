12c3b9885ec314b61dfed672030337064878f43c
Diane KL <dianekimlim@gmail.com>
2018-01-23 18:27:56 +0000

    updating functionality by separating ejs files and integrating body-parser

--- express-server.js
+++ express-server.js
@@ -1,19 +1,34 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/urls", (req, res) => {
+	let templateVars = { urls: urlDatabase };
+	res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+	let templateVars = { shortURL: req.params.id };
+	res.render("urls_show", templateVars);
+});
+
 app.get("/", (req, res) => {
   res.end("Hello! This is the Example.");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
 
 app.get("/hello", (req, res) => {
 	res.end(`<html>
