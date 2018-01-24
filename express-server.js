974f37118c62f0d7b3b558bb3778490d2a6e223d
Diane KL <dianekimlim@gmail.com>
2018-01-24 17:20:54 +0000

    added cookies for username logins on each views page
    modified .js file to link them together

--- express-server.js
+++ express-server.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+var PORT = process.env.PORT || 8080; 
 const bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+
 
 app.set("view engine", "ejs");
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 
 var urlDatabase = {
@@ -15,13 +18,15 @@ app.get("/", (req, res) => {
   res.end("Hello! This is the Example.");
 });
 
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
 app.get("/urls", (req, res) => {
 	let templateVars = { 
-		urls: urlDatabase 
+		username: req.cookies["username"], 
+		urls: urlDatabase
 	};
 	res.render("urls_index", templateVars);
 });
@@ -34,12 +39,16 @@ app.post("/urls", (req, res) => {
 
 
 app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
+	let templateVars = {
+		username: req.cookies["username"]
+	};
+    res.render("urls_new", templateVars);
 });
 
 
 app.get("/urls/:id", (req, res) => {
 	let templateVars = { 
+		username: req.cookies["username"], 
 		shortURL: req.params.id,
 		longURL: urlDatabase[req.params.id] 
 	};
@@ -56,6 +65,13 @@ app.post("/urls/:id", (req, res) => {
 app.post("/urls/:id/delete", (req, res) => {
 	delete urlDatabase[req.params.id]
 	delete req.params.id 
+	res.redirect("http://localhost:8080/urls");
+})
+
+
+app.post("/login", (req, res) => {
+	res.cookie('username', req.body.username);
+	console.log(req.cookies);
 	res.redirect("http://localhost:8080/urls");
 })
 
